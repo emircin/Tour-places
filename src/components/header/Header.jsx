6907b21388811d1ad2bf8264a4d5516275787400
emircingiloglu <emircingiloglu@gmail.com>
2022-03-20 20:58:38 +0300

    react project done

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <div>
+        <header>
+          <p className='h1 text-center p-5'>POPULAR TOUR PLACES</p>
+        </header>
+    </div>
+  )
+}
+
+export default Header
